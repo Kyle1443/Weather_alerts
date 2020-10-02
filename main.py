d66813259e5804077fa42171f771aae6d92569e0
Kyle <kyle1443@gmail.com>
2020-10-02 22:28:48 +0100

    comments
    added comments to describe how the code works

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import time
 import schedule
 
 
-
+#makes the request to the webpage
 source = req.get('https://weather.com/en-GB/weather/today/l/51.55,-0.43?par=google&temp=c')
 soup = BeautifulSoup(source.text, 'html.parser')
 
+#scrapes the page and pulls the desired information from HTML
+#uses the .strip() method so the numbers can be turned into intergers
 current_conditions = soup.find(id = 'WxuHourlyWeatherCard-main-29584a07-3742-4598-bc2a-f950a9a4d900')
 current_temp = current_conditions.find(class_ = '_2v_go').text
 temp_number = int(current_temp.strip('°'))
@@ -18,17 +20,17 @@ temp_number = int(current_temp.strip('°'))
 rain_chance = current_conditions.find(class_ = '_2H5Iw').text
 chance_percent = int(rain_chance.strip('%'))
 
-#extra_details = soup.find(id = 'todayDetails')
-#current_wind = extra_details.find(class_ = '_1Va1P undefined').text
-#wind_number = int(current_wind.strip('km/h'))
+extra_details = soup.find(id = 'todayDetails')
+current_wind = extra_details.find(class_ = '_1Va1P undefined').text
+wind_number = int(current_wind.strip('km/h'))
 
-
+#array of strings for the email message. function will select them based on outside conditions
 update_text = [
     'Warmer than usual.' + ' ' + str(temp_number) + '°C outside.',
     'Temp isnt too bad at the moment. Wear a jacket though.' + ' ' + str(temp_number) + '°C outside.',
     'Its cold out bro. Might wanna layer up.' + ' ' + str(temp_number) + '°C outside.',
     'Its freezing out...' + ' ' + str(temp_number) + '°C outside.',
-    'Remember that snow planet on Star Wars? Yeah thats what outside is like right now.' + ' ' + str(temp_number) + '°C outside.',
+    'Dangerously cold. Be careful out there.' + ' ' + str(temp_number) + '°C outside.',
 
     ' Also, clear skies. No need to worry about rain.' + ' ' + str(rain_chance) + ' ' + 'chance.',
     ' Also, probably isnt going to rain, but keep an eye out.' + ' ' + str(rain_chance) + ' ' + 'chance.', 
@@ -36,9 +38,12 @@ update_text = [
     ' Also, definitely gonna rain. Make sure you have your gear.' + ' ' + str(rain_chance) + ' ' + 'chance.',
 ]
 
+#global variables to be manipulated by functions:
+#makes it easier to use with email method
 temp_result = 'placeholder text'
 rain_result = 'placeholder text'
 
+#function to check temperature and pick email message string based on result
 def temp_check():
     global temp_result
     if temp_number > 15:
@@ -56,6 +61,7 @@ def temp_check():
     return
 
 
+#function to check chance of rain and pick email message string based on result
 def rain_check():
     global rain_result
     if chance_percent < 10:
@@ -76,10 +82,11 @@ rain_check()
 
 time.sleep(2)
 
-
+#grabs email and password from environment variables
 EMAIL_ADRESS = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
+#method to send results to my email as a weather alert
 msg = EmailMessage()
 msg['Subject'] = 'Weather alert'
 msg['From'] = EMAIL_ADRESS
